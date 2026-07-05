7725a51965f274308b662aac5f8910329ce9d39c
RSKyo <nimeimiao@gmail.com>
2026-07-06 02:51:43 +0800

    cli

--- cli.js
+++ cli.js
@@ -1,21 +1,14 @@
 #!/usr/bin/env node
-import { log } from "./infra/log.js";
 import { resolveCommand } from "./infra/cmd.js";
 import { run } from "./infra/runner.js";
+import { log } from "./infra/log.js";
+import { COMMAND_GROUPS } from "./cmd/_cmd.js";
 
-import { closeCdpClients } from "./cdp/lifecycle.js";
-
-import { CHROME_COMMANDS } from "./cmd/chrome.js";
-import { CLIPBOARD_COMMANDS } from "./cmd/clipboard.js";
-
-// 一级命令分组
-const COMMAND_GROUPS = {
-  chrome: CHROME_COMMANDS,
-  clipboard: CLIPBOARD_COMMANDS,
-};
+import { closeCdpClients } from "./cdp/client.js";
 
 const json = process.argv.includes("--json");
 const stack = process.argv.includes("--stack");
+
 const runOptions = {
   json,
   stack,
---
+++ cmd/_cmd.js
@@ -0,0 +1,7 @@
+import { CHROME_COMMANDS } from "./chrome.js";
+import { CLIPBOARD_COMMANDS } from "./clipboard.js";
+
+export const COMMAND_GROUPS = {
+  chrome: CHROME_COMMANDS,
+  clipboard: CLIPBOARD_COMMANDS,
+};
